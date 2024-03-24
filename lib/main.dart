5aa4791a4d9aa4a41e0b545095e15ae3a6c04f2f
Buddhi tamang <102455058+buddhitamang@users.noreply.github.com>
2024-03-24 14:10:00 +0545

    updated projects

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movieapp/Models/movies_provider.dart';
 import 'package:movieapp/Pages/home_page.dart';
+import 'package:movieapp/Pages/main_page.dart';
 import 'package:provider/provider.dart';
 
 import 'firebase_options.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: MainPage(),
       debugShowCheckedModeBanner: false,
     );
   }
